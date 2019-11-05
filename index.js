336ac2ae0c126ae13187b4bc4ca407074b0b90a8
tanyashulha <tsulga6@gmail.com>
2019-11-05 21:15:09 +0300

    feat: add keyboard buttons with values

--- index.js
+++ index.js
@@ -8,11 +8,81 @@ const keyboardContainer = document.createElement('div');
 keyboardContainer.className = 'keyboard';
 document.body.append(keyboardContainer);
 
-keyboardContainer.insertAdjacentHTML('afterbegin', '<button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button big-button"></button><button class="button middle-button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button middle-button"></button><button class="button big-button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button big-button"></button><button class="button big-button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button"></button><button class="button middle-button"></button><button class="button"></button><button class="button"></button><button class="button space-button"></button><button class="button"></button><button class="button middle-button"></button><button class="button"></button><button class="button"></button><button class="button"></button>');
+function getButton(value, className) {
+  return `<button class="button ${className}">${value}</button>`;
+}
 
-const toggleButtons = [...document.getElementsByClassName('button')];
-toggleButtons.forEach((toggleButton) => {
-    toggleButton.onclick = () => {
-        toggleButton.classList.toggle('active');
-    }
-})
+let buttons = '';
+const buttonsData = [
+  {value: 'ё'},
+  {value: 1},
+  {value: 2},
+  {value: 3},
+  {value: 4},
+  {value: 5},
+  {value: 6},
+  {value: 7},
+  {value: 8},
+  {value: 9},
+  {value: 0},
+  {value: '-'},
+  {value: '='},
+  {value: 'Backspace', className: 'big-button'},
+  {value: 'Tab', className: 'middle-button'},
+  {value: 'й'},
+  {value: 'ц'},
+  {value: 'у'},
+  {value: 'к'},
+  {value: 'е'},
+  {value: 'н'},
+  {value: 'г'},
+  {value: 'ш'},
+  {value: 'щ'},
+  {value: 'з'},
+  {value: 'х'},
+  {value: 'ъ'},
+  {value: '\\'},
+  {value: 'DEL', className: 'middle-button'},
+  {value: 'Caps Lock', className: 'big-button'},
+  {value: 'ф'},
+  {value: 'ы'},
+  {value: 'в'},
+  {value: 'а'},
+  {value: 'п'},
+  {value: 'р'},
+  {value: 'о'},
+  {value: 'л'},
+  {value: 'д'},
+  {value: 'ж'},
+  {value: 'э'},
+  {value: 'ENTER', className: 'big-button'},
+  {value: 'Shift', className: 'big-button'},
+  {value: '\\'},
+  {value: 'я'},
+  {value: 'ч'},
+  {value: 'с'},
+  {value: 'м'},
+  {value: 'и'},
+  {value: 'т'},
+  {value: 'ь'},
+  {value: 'б'},
+  {value: 'ю'},
+  {value: '/'},
+  {value: '↑'},
+  {value: 'Shift', className: 'middle-button'},
+  {value: 'Ctrl', className: 'middle-button'},
+  {value: 'Win', className: 'middle-button'},
+  {value: 'Alt', className: 'middle-button'},
+  {value: '', className: 'space-button'},
+  {value: 'Alt', className: 'middle-button'},
+  {value: 'Ctrl', className: 'middle-button'},
+  {value: '←'},
+  {value: '↓'},
+  {value: '→'},
+];
+
+buttonsData.forEach(buttonData => {
+  buttons += getButton(buttonData.value, buttonData.className);
+});
+
+keyboardContainer.insertAdjacentHTML('beforeend', buttons);
