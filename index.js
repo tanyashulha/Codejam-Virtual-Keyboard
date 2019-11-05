f1de2b898d43218fca4fdcdb63dbb40b273b5be3
tanyashulha <tsulga6@gmail.com>
2019-11-05 11:02:50 +0300

    feat: implement textarea

---
+++ index.js
@@ -0,0 +1,5 @@
+let textField = document.createElement('textarea');
+textField.id = 'text-field';
+textField.setAttribute('rows', '10');
+textField.setAttribute('disabled', 'disabled');
+document.body.append(textField);
