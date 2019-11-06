86b670f4c9becace4cb8d3c55d45236781fe0b4f
tanyashulha <tsulga6@gmail.com>
2019-11-06 11:53:50 +0300

    feat: add properties for en language

--- index.js
+++ index.js
@@ -9,88 +9,376 @@ keyboardContainer.className = 'keyboard';
 document.body.append(keyboardContainer);
 
 function getButton(valueEn, className) {
-  return `<button class="button ${className}">${valueEn}</button>`;
+  if (className !== undefined) {
+    return `<button class="button ${className}">${valueEn}</button>`;
+  } else {
+    return `<button class="button">${valueEn}</button>`;
+  }
 }
-
+  
 let buttons = '';
 const buttonsData = [
-  {valueEn: '`'},
-  {valueEn: 1},
-  {valueEn: 2},
-  {valueEn: 3},
-  {valueEn: 4},
-  {valueEn: 5},
-  {valueEn: 6},
-  {valueEn: 7},
-  {valueEn: 8},
-  {valueEn: 9},
-  {valueEn: 0},
-  {valueEn: '-'},
-  {valueEn: '='},
-  {valueEn: 'Backspace', className: 'big-button'},
-  {valueEn: 'Tab', className: 'middle-button'},
-  {valueEn: 'q'},
-  {valueEn: 'w'},
-  {valueEn: 'e'},
-  {valueEn: 'r'},
-  {valueEn: 't'},
-  {valueEn: 'y'},
-  {valueEn: 'u'},
-  {valueEn: 'i'},
-  {valueEn: 'o'},
-  {valueEn: 'p'},
-  {valueEn: '['},
-  {valueEn: ']'},
-  {valueEn: '\\'},
-  {valueEn: 'Del', className: 'middle-button'},
-  {valueEn: 'Caps Lock', className: 'big-button'},
-  {valueEn: 'a'},
-  {valueEn: 's'},
-  {valueEn: 'd'},
-  {valueEn: 'f'},
-  {valueEn: 'g'},
-  {valueEn: 'h'},
-  {valueEn: 'j'},
-  {valueEn: 'k'},
-  {valueEn: 'l'},
-  {valueEn: ';'},
-  {valueEn: '&#8217;'},
-  {valueEn: 'Enter', className: 'big-button'},
-  {valueEn: 'Shift', className: 'big-button'},
-  {valueEn: '\\'},
-  {valueEn: 'z'},
-  {valueEn: 'x'},
-  {valueEn: 'c'},
-  {valueEn: 'v'},
-  {valueEn: 'b'},
-  {valueEn: 'n'},
-  {valueEn: 'm'},
-  {valueEn: ','},
-  {valueEn: '.'},
-  {valueEn: '/'},
-  {valueEn: '↑'},
-  {valueEn: 'Shift', className: 'middle-button'},
-  {valueEn: 'Ctrl', className: 'middle-button'},
-  {valueEn: 'Win', className: 'middle-button'},
-  {valueEn: 'Alt', className: 'middle-button'},
-  {valueEn: '', className: 'space-button'},
-  {valueEn: 'Alt', className: 'middle-button'},
-  {valueEn: 'Ctrl', className: 'middle-button'},
-  {valueEn: '←'},
-  {valueEn: '↓'},
-  {valueEn: '→'},
+  {
+    en: {
+      valueEn: '`',
+      keyCode: '192'
+    }
+  },
+  {
+    valueEn: 1,
+    keyCode: '49'
+  },
+  {
+    valueEn: 2,
+    keyCode: '50'
+  },
+  {
+    valueEn: 3,
+    keyCode: '51'
+  },
+  {
+    valueEn: 4,
+    keyCode: '52'
+  },
+  {
+    valueEn: 5,
+    keyCode: '53'
+  },
+  {
+    valueEn: 6,
+    keyCode: '54'
+  },
+  {
+    valueEn: 7,
+    keyCode: '55'
+  },
+  {
+    valueEn: 8,
+    keyCode: '56'
+  },
+  {
+    valueEn: 9,
+    keyCode: '57'
+  },
+  {
+    valueEn: 0,
+    keyCode: '48'
+  },
+  {
+    valueEn: '-',
+    keyCode: '189'
+  },
+  {
+    valueEn: '=',
+    keyCode: '187'
+  },
+  {
+    valueEn: 'Backspace',
+    className: 'big-button',
+    keyCode: '8'
+  },
+  {
+    valueEn: 'Tab',
+    className: 'middle-button',
+    keyCode: '9'
+  },
+  {
+    en: {
+      valueEn: 'q',
+      keyCode: '81'
+    }
+  },
+  {
+    en: {
+      valueEn: 'w',
+      keyCode: '87'
+    }
+  },
+  {
+    en: {
+      valueEn: 'e',
+      keyCode: '69'
+    }
+  },
+  {
+    en: {
+      valueEn: 'r',
+      keyCode: '82'
+    }
+  },
+  {
+    en: {
+      valueEn: 't',
+      keyCode: '84'
+    }
+  },
+  {
+    en: {
+      valueEn: 'y',
+      keyCode: '89'
+    }
+  },
+  {
+    en: {
+      valueEn: 'u',
+      keyCode: '85'
+    }
+  },
+  {
+    en: {
+      valueEn: 'i',
+      keyCode: '73'
+    }
+  },
+  {
+    en: {
+      valueEn: 'o',
+      keyCode: '79'
+    }
+  },
+  {
+    en: {
+      valueEn: 'p',
+      keyCode: '80'
+    }
+  },
+  {
+    en: {
+      valueEn: '[',
+      keyCode: '219'
+    }
+  },
+  {
+    en: {
+      valueEn: ']',
+      keyCode: '221'
+    }
+  },
+  {
+    en: {
+      valueEn: '\\',
+      keyCode: '220'
+    }
+  },
+  {
+    valueEn: 'Del', 
+    className: 'middle-button',
+    keyCode: '46'
+  },
+  {
+    valueEn: 'Caps Lock', 
+    className: 'big-button',
+    keyCode: '20'
+  },
+  {
+    en: {
+      valueEn: 'a',
+      keyCode: '65'
+    }
+  },
+  {
+    en: {
+      valueEn: 's',
+      keyCode: '83'
+    }
+  },
+  {
+    en: {
+      valueEn: 'd',
+      keyCode: '68'
+    }
+  },
+  {
+    en: {
+      valueEn: 'f',
+      keyCode: '70'
+    }
+  },
+  {
+    en: {
+      valueEn: 'g',
+      keyCode: '71'
+    }
+  },
+  {
+    en: {
+      valueEn: 'h',
+      keyCode: '72'
+    }
+  },
+  {
+    en: {
+      valueEn: 'j',
+      keyCode: '74'
+    }
+  },
+  {
+    en: {
+      valueEn: 'k',
+      keyCode: '75'
+    }
+  },
+  {
+    en: {
+      valueEn: 'l',
+      keyCode: '76'
+    }
+  },
+  {
+    en: {
+      valueEn: ';',
+      keyCode: '186'
+    }
+  },
+  {
+    en: {
+      valueEn: '&#8217;',
+      keyCode: '222'
+    }
+  },
+  {
+    valueEn: 'Enter', 
+    className: 'big-button',
+    keyCode: '13'
+  },
+  {
+    valueEn: 'Shift', 
+    className: 'big-button',
+    keyCode: '16'
+  },
+  {
+    en: {
+      valueEn: '\\',
+      keyCode: '220'
+    }
+  },
+  {
+    en: {
+      valueEn: 'z',
+      keyCode: '90'
+    }
+  },
+  {
+    en: {
+      valueEn: 'x',
+      keyCode: '88'
+    }
+  },
+  {
+    en: {
+      valueEn: 'c',
+      keyCode: '67'
+    }
+  },
+  {
+    en: {
+      valueEn: 'v',
+      keyCode: '86'
+    }
+  },
+  {
+    en: {
+      valueEn: 'b',
+      keyCode: '66'
+    }
+  },
+  {
+    en: {
+      valueEn: 'n',
+      keyCode: '78'
+    }
+  },
+  {
+    en: {
+      valueEn: 'm',
+      keyCode: '77'
+    }
+  },
+  {
+    en: {
+      valueEn: ',',
+      keyCode: '188'
+    }
+  },
+  {
+    en: {
+      valueEn: '.',
+      keyCode: '190'
+    }
+  },
+  {
+    en: {
+      valueEn: '/',
+      keyCode: '191'
+    }
+  },
+  {  
+    valueEn: '↑',
+    keyCde: '38'
+  },
+  {
+    valueEn: 'Shift', 
+    className: 'middle-button',
+    keyCode: '16'
+  },
+  {
+    valueEn: 'Ctrl', 
+    className: 'middle-button',
+    keyCode: '17'
+  },
+  {
+    valueEn: 'Win', 
+    className: 'middle-button',
+    keyCode: '91'
+  },
+  {
+    valueEn: 'Alt',
+    className: 'middle-button',
+    keyCode: '18'
+    },
+  {
+    valueEn: '', 
+    className: 'space-button',
+    keyCode: '32'
+  },
+  {
+    valueEn: 'Alt', 
+    className: 'middle-button',
+    keyCode: '18'
+  },
+  {
+    valueEn: 'Ctrl', 
+    className: 'middle-button',
+    keyCode: '17'
+  },
+  {
+    valueEn: '←',
+    keyCode: '37'
+  },
+  {
+    valueEn: '↓',
+    keyCode: '40'
+  },
+  {
+    valueEn: '→',
+    keyCode: '39'
+  },
 ];
 
-buttonsData.forEach(buttonData => {
-  buttons += getButton(buttonData.valueEn, buttonData.className);
+buttonsData.forEach((buttonData) => {
+  if (buttonData.en !== undefined) {
+    buttons += getButton(buttonData.en.valueEn, buttonData.className);
+  } else {
+    buttons += getButton(buttonData.valueEn, buttonData.className);
+  }
 });
 
 keyboardContainer.insertAdjacentHTML('beforeend', buttons);
 
 function getActiveButton() {
-
+  
 }
 
 document.addEventListener('keydown', (e) => {
-  console.log('нажаф:' + e.key);
+    
 })
